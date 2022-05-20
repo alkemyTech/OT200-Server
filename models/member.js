30e5b3ad8de61128e6935a0bb19c30f765586ef5
gtiavo <victorg.avila@gmail.com>
2022-05-20 19:32:52 -0300

    cree Model member

---
+++ models/member.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Member extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+        //TODO: asociación con modelos
+    }
+  };
+  Member.init({
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    facebookUrl: DataTypes.STRING,
+    instagramUrl: DataTypes.STRING,
+    linkedinUrl: DataTypes.STRING,
+    image: DataTypes.STRING,
+    description: DataTypes.DATE,
+    deletedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Member',
+  });
+  return Member;
+};
