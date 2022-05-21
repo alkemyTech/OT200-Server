90513b1a9017120700ed7723a3530c20621ed107
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-05-21 17:46:23 -0300

    role model timestamps and roles controller try catch refactor

--- controllers/roles.controller.js
+++ controllers/roles.controller.js
@@ -1,8 +1,13 @@
 const modelRole = require('../models').Role;
 
 const createRole = async (req, res) => {
-    const newRole = await modelRole.create(req.body);
-    return res.status(200).json({error: false, message: "Role created successfully", data: newRole});
+    try{
+        const newRole = await modelRole.create(req.body);
+        return res.status(200).json({ error: false, message: "Role created successfully", data: newRole});
+    }
+    catch (error) {
+        return res.status(409).json({ error: true, message: `Failed to create new role. Error: ${error}` });
+    }
 }
 
 module.exports = { createRole };
--- models/role.js
+++ models/role.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Role',
+    timestamps: true,
     paranoid: true
   });
   return Role;
