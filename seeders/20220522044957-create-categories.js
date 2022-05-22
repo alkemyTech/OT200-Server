094e14cce60cb4a698d4f1805f10547142c58843
Robert Santos <rssp_96@hotmail.com>
2022-05-22 00:03:14 -0500

    create models, seeders and migrations

---
+++ seeders/20220522044957-create-categories.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "categories",
+      [
+        {
+          name: "Dummy cat",
+          description: "Random description",
+          image:
+            "https://images.unsplash.com/photo-1507100654232-fb6b007a0237?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    
+    await queryInterface.bulkDelete('categories', null, {});
+     
+  },
+};
