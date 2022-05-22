18e1066997b43286f7093178f8a9074dcd466cc8
Robert Santos <rssp_96@hotmail.com>
2022-05-22 15:13:08 -0500

    create model

---
+++ models/news.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class New extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  New.init(
+    {
+      name: DataTypes.STRING,
+      content: DataTypes.TEXT,
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "news",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  return New;
+};
