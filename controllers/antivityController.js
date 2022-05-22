20fe4ba087655c2903d4dcbcd5381e5f0ff2ff46
Luciano Dato <lucianodato82@gmail.com>
2022-05-22 12:14:14 -0300

    OT200-21-crear-modelo-migracion-y-controlador-base-de-activities

---
+++ controllers/antivityController.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+const Activity = db.activity;
+
+const create = (req, res) => {
+    
+};
+
+const getAll = (req, res) => {
+
+};
+
+const getOne = (req, res) => {
+
+};
+
+const update = (req, res) => {
+
+};
+
+const deleteOne = (req, res) => {
+
+};
+
+module.exports = activityController;
