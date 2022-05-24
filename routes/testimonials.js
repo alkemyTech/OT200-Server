6b29895d333dc04a26b4818e54da9d8ed9cb5377
gabriel <gamcode98@gmail.com>
2022-05-23 22:29:34 -0300

    OT200-20 Testimonial model identation fixed, base controller modified

--- routes/testimonials.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  return res.json({ message: "success" });
-});
-
-module.exports = router;
