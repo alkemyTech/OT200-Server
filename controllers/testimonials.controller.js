59d2a266a1adee6d1b2b7466a71efa741d835fe4
Gabriel Alejandro Mamani <78627015+gamcode98@users.noreply.github.com>
2022-05-27 20:41:09 -0300

    Ot200 20 create model migration and  base controller (#15)
    * OT200-20 Testimonial model, migration and base controller
    
    * OT200-20 Testimonial model, migration fixed
    
    * OT200-20 Testimonial model identation fixed, base controller modified
    
    * OT200-20 Deleted unnecessary lines on app.js
    
    * OT200-20 changes solved
    
    * OT200-20 devDependence uninstalled

---
+++ controllers/testimonials.controller.js
@@ -0,0 +1,17 @@
+const getAllTestimonials = (req, res) => {};
+
+const getOneTestiominal = (req, res) => {};
+
+const createTestimonial = (req, res) => {};
+
+const updateTestimonial = (req, res) => {};
+
+const deleteTestimonial = (req, res) => {};
+
+module.exports = {
+    getAllTestimonials,
+    getOneTestiominal,
+    createTestimonial,
+    updateTestimonial,
+    deleteTestimonial,
+};
