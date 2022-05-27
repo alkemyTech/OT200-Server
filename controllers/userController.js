41bfd7ff541217586464e2e300adf7764e25593e
Mario Martinez <mariomtz091@gmail.com>
2022-05-27 19:51:03 -0300

    Correcion en userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,40 +6,47 @@ const {generateToken} = require('../middleware/userToken');
 const userController = {
     login: (req, res) => {
         let validations = validationResult(req);
-        if(validations.isEmpty()){
-            const {email, password} = req.body;            
-            try{            
-                let user = await db.User.findOne({                    
-                    where: {email}
-                });            
-                if(user){ 
-                    const isValidPassword = await bcrypt.compareSync(password, user.password);
-                    if(isValidPassword){                        
-                        const token = generateToken(user);
-                        res.status(200).json({
-                            message: 'Login exitoso',
-                            token
-                        });
-                    }else{                    
-                        res.status(401).json({
-                            ok: 'False',
-                            message: 'Contraseña incorrecta'
-                        });
-                    }
-                }else{                    
-                    res.status(401).json({
-                        ok: 'False',
-                        message: 'Usuario no encontrado'
-                    });
-                }
-            }catch(error){
-                res.status(500).json({
-                    message: 'Ocurrio un error',
-                    error
-                });
-            }
+
+        if(!validations.isEmpty()){
+            return res.status(400).json({
+                message: 'Error',
+            });
         }
-    }
+
+        const {email, password} = req.body;
+
+        try{
+            let user = await db.User.findOne({
+                where: {
+                    email
+                }
+            });
+
+            if(!user)res.status(404).json({message: 'Usuario no encontrado', ok: false});
+        
+            let isValidPassword = await bcrypt.compareSync(password, user.password);
+
+            if(!isValidPassword){
+                return res.status(400).json({
+                    message: 'Contraseña incorrecta',
+                    ok: false
+                })
+            }else{
+                let token = generateToken(user);                
+
+                res.status(200).json({
+                    message: 'Login exitoso',
+                    token
+                })
+            }  
+        }
+        catch(error){
+            return res.status(500).json({
+                message: 'Error',
+                data: error
+            });
+        }
+    },
 }
 
 module.exports = userController
