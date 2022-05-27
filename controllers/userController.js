b5d793b8849df3e5b6da3b0465e7f027baa5bd5f
Mario Martinez <mariomtz091@gmail.com>
2022-05-27 19:16:29 -0300

    Correcion de userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,40 +5,37 @@ const db = require('../database/models');
 const userController = {
     login: (req, res) => {
         let validations = validationResult(req);
-        if(validations.isEmpty()){
-            const {email, password} = req.body;
-            try{
-                let user = await db.User.findOne({
-                    where: {email}
-                });
-                if(user){
-                    const isValidPassword = await bcrypt.compareSync(password, user.password);
-                    if(isValidPassword){
-                        res.status(200).json({
-                            message: 'Login exitoso',
-                            user: user
-                        });
-                    }else{
-                        res.status(401).json({
-                            ok: 'False'
-                        });
 
-                    }
-                }else{
-                    res.status(401).json({
-                        ok: 'False'
-                    });
-                }
-            }catch(error){
-                res.status(500).json({
-                    message: 'Ocurrio un error',
-                    error
-                });
-            }
-            
+        if(!validations.isEmpty()){
+            return res.status(400).json({
+                message: 'Error',
+            });
         }
 
-    }
+        const {email, password} = req.body;
+
+        try{
+            let user = await db.User.findOne({
+                where: {
+                    email
+                }
+            });
+
+            if(!user)res.status(404).json({message: 'Usuario no encontrado', ok: false});
+        
+            let isValidPassword = await bcrypt.compareSync(password, user.password);
+
+            if(!isValidPassword) res.status(401).json({message: 'Contraseña incorrecta', ok: false});
+            
+            res.status(200).json({message: 'Login exitoso', data: {user, token}});
+
+        }catch(error){
+            return res.status(500).json({
+                message: 'Error',
+                data: error
+            });
+        }
+    },
 }
 
 module.exports = userController
