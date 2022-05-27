1845bbaf249a44e1884154b97ee18114ee1a0060
Luciano Dato <lucianodato82@gmail.com>
2022-05-27 14:19:09 -0300

    Se corrigio secret y se guarda user en objeto req

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,17 +1,17 @@
 
 const JWT = require('jsonwebtoken');
-const secret = process.env.SECRET;
 
 const verifyToken = (req, res, next) => {
     const token = req.headers['x-access-token'];
     if (!token) {
         res.status(401).send({error: "No se ha enviado el token de autenticación"});
     } else {
-        JWT.verify(token, secret, async (error) => {
+        JWT.verify(token, process.env.SECRET, async (error, user) => {
             if (error) {
                 res.status(400).send({message: "Token invalido"});
             } else {
                 try {
+                    req.user = user;
                     next();
                 } catch (error) {
                     res.send(error);
