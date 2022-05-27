2d0b08934144fda51cf089ce83c27d48056fe9b7
Luciano Dato <lucianodato82@gmail.com>
2022-05-27 18:13:47 -0300

    Se corrige else de middleware

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,19 +4,14 @@ const JWT = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     const token = req.headers['x-access-token'];
     if (!token) {
-        res.status(401).send({error: "No se ha enviado el token de autenticación"});
+        res.status(401).send({ error: "No se ha enviado el token de autenticación" });
     } else {
         JWT.verify(token, process.env.SECRET, async (error, user) => {
             if (error) {
-                res.status(400).send({message: "Token invalido"});
-            } else {
-                try {
-                    req.user = user;
-                    next();
-                } catch (error) {
-                    res.send(error);
-                }
-            }
+                res.status(400).send({ message: "Token invalido" });
+            };
+            req.user = user;
+            next();
         })
     }
 }
