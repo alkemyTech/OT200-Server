35c492e8cd18e2c89f8c0f32ba99706d3001340f
gabriel <gamcode98@gmail.com>
2022-05-27 23:09:46 -0300

    OT200-24 data layer added, app.js fixed and nodemon devDependency uninstalled

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,20 +1,16 @@
-const bcrypt = require("bcrypt");
-const db = require("./../models/");
+const create = require("../services/user");
 
 const createUser = async (req, res) => {
     try {
+
         const data = req.body;
-        const hash = await bcrypt.hash(data.password, 10);
-
-        const newUser = await db.sequelize.models.User.create({
-            ...data,
-            password: hash,
-        });
-
-        delete newUser.dataValues.password;
+        const newUser = await create(data)
+        
         return res.json(newUser);
+
     } catch (error) {
-        return res.status(400).json({
+
+        return res.status(500).json({
             error: true,
             message: "Something was wrong",
         });
