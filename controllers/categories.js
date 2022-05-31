92fc143aa03f34fe9ca5eba22999d9543cf65a25
gtiavo <victorg.avila@gmail.com>
2022-05-31 19:38:45 -0300

    add controller categories-createCategory

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,9 +1,12 @@
 const db = require("../models/index");
+
 class CategoryController {
 
-
-
-
+//Metodo POST/ TODO:crear una nueva categoria en DB
+ static createCategory(req, res){
+      const { name } = req.body;
+      res.json({ msg:'Crear categoria', name})
+}
 
   
 }
