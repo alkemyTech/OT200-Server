60046b3f5e18906b79df596b8df5a7c5cc84a1b6
Mario Martinez <mariomtz091@gmail.com>
2022-05-31 16:05:43 -0300

    Se elimino lo que no corresponde y correcciones

--- middleware/userToken.js
+++ middleware/userToken.js
@@ -5,23 +5,30 @@ const generateToken = (user) => {
         id: user.id,
         roleId: user.roleId,
     };
+
     const expire = {
         expiresIn: '1d'
     };
+
     return jwt.sign(tokenSign, process.env.SECRET_KEY, expire);
 }
 
 const userVerify = async (req, res, next) => {
     const token = req.headers['authorization'];
+
     if(!token){
         return res.status(401).json({
             message: 'No se encontro el token'
         });
     }
+
     try{
         const decoded = await jwt.verify(token, process.env.SECRET_KEY);
+
         req.user = decoded;
+
         next();
+
     }catch(error){
         return res.status(401).json({
             message: 'Token invalido'
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,15 @@
-const express = require('express');
+const validatorHandler = require("../middlewares/validatorHandler");
+const registrationSchema = require("./../schemas/user");
+const express = require("express");
+const { checkSchema } = require("express-validator");
+const { createUser } = require("../controllers/auth.controller");
+const {generateToken, userVerify} = require("../middlewares/userToken");
 const router = express.Router();
-const userController = require('../controllers/userController');
 
-router.post('/login', userController.login)
+router.post('/login',generateToken, userVerify)
+router.post(
+  "/register",
+  validatorHandler(checkSchema(registrationSchema)),
+  createUser
+);
+module.exports = router;
