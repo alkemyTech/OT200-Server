9bc9de5209de856af58036797d2ee090a60d3f00
Mario Martinez <mariomtz091@gmail.com>
2022-06-01 18:40:15 -0300

    Correcion en req.headers

--- middleware/userToken.js
+++ middleware/userToken.js
@@ -14,7 +14,7 @@ const generateToken = (user) => {
 }
 
 const userVerify = async (req, res, next) => {
-    const token = req.headers['authorization'];
+    const token = req.headers.authorization.split(" ")[1];
 
     if(!token){
         return res.status(401).json({
