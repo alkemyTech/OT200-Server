bfbf146308c1180cad4f1462ce99745dbd8814e4
Mario Martinez <mariomtz091@gmail.com>
2022-06-01 20:49:44 -0300

    correcion en userToken y routes

--- middleware/userToken.js
+++ middleware/userToken.js
@@ -12,31 +12,7 @@ const generateToken = (user) => {
 
     return jwt.sign(tokenSign, process.env.SECRET_KEY, expire);
 }
-
-const userVerify = async (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-
-    if(!token){
-        return res.status(401).json({
-            message: 'No se encontro el token'
-        });
-    }
-
-    try{
-        const decoded = await jwt.verify(token, process.env.SECRET_KEY);
-
-        req.user = decoded;
-
-        next();
-
-    }catch(error){
-        return res.status(401).json({
-            message: 'Token invalido'
-        });
-    }
-}
-
-    
+  
 
 
-module.exports = {generateToken, userVerify};
+module.exports = generateToken;
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,9 @@ const registrationSchema = require("./../schemas/user");
 const express = require("express");
 const { checkSchema } = require("express-validator");
 const { createUser } = require("../controllers/auth.controller");
-const {generateToken, userVerify} = require("../middleware/userToken");
 const router = express.Router();
 
-router.post('/login',generateToken, userVerify)
+
 router.post(
   "/register",
   validatorHandler(checkSchema(registrationSchema)),
