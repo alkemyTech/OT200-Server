76dc2910a9e6d8b9d8f9fa4a7723cbd250b859c9
Mario Martinez <mariomtz091@gmail.com>
2022-06-01 00:40:46 -0300

    uso de validatorHandler

--- middleware/validLogin.js
+++ middleware/validLogin.js
@@ -1,23 +1,11 @@
-const {body, validationResult} = require('express-validator');
+const {body} = require('express-validator');
 
 const validLogin = [        
     body('email').notEmpty().withMessage('Obligatorio').isEmail().withMessage('Ingrese un email valido.'),
     body('password').notEmpty().withMessage('Obligatorio').isLength({min:8}).withMessage('La contraseña debe tener al menos 8 caracteres.')
 ]
 
-const validations = (req, res, next) => {
-    let errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            message: 'Error',
-            errors: errors.array() 
-        });
-    }
-    next();
-}
 
 
 
-
-module.exports = {validLogin, validations};
+module.exports = {validLogin};
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const {validations,validLogin} = require('../middleware/validLogin');
+const validatorHandler = require("../middlewares/validatorHandler");
+const registrationSchema = require("./../schemas/user");
+const express = require("express");
+const { checkSchema } = require("express-validator");
+const { createUser } = require("../controllers/auth.controller");
+const {validLogin} = require('../middleware/validLogin');
 const userController = require('../controllers/users.controller');
+const router = express.Router();
 
-router.post('/login', validations(validLogin), userController.login)
 
+router.post('/login', validatorHandler(validLogin), userController.login)
+
+router.post(
+  "/register",
+  validatorHandler(checkSchema(registrationSchema)),
+  createUser
+);
 module.exports = router;
