d29871b16b6d639db1de98d27f0a0fc30799dd4b
Abel Martín Bergesio <martinbergesio@gmail.com>
2022-06-01 00:29:12 -0300

    verifyToken and checkAdmin added to endpoint

--- routes/slides.js
+++ routes/slides.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { createNewSlide } = require('../controllers/slides.controller');
 const { upload } = require('../middleware/multer');
+const verifyToken = require('../middleware/verifyToken');
+const checkAdmin = require('../middleware/checkAdmin');
 
 //multer saves slide in public/slides folder
-router.post('/', upload.single('slide'), createNewSlide);
+router.post('/', verifyToken, checkAdmin, upload.single('slide'), createNewSlide);
 
 module.exports = router;
