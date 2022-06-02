76ac0dc765d01daa3d75099ad6e0363f8e3f6bf6
gabriel <gamcode98@gmail.com>
2022-06-02 12:23:52 -0300

    controller, validation, route and service added to testimonial

---
+++ routes/testimonials.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const { createTestimonial } = require("../controllers/testimonials.controller");
+const validatorHandler = require("../middleware/validatorHandler");
+const { checkSchema } = require("express-validator");
+const testimonialSchema = require("../schemas/testimonial");
+const verifyToken = require("./../middleware/verifyToken")
+const checkAdmin = require("../middleware/checkAdmin");
+
+router.post(
+    "/",
+    // verifyToken,
+    // checkAdmin,
+    validatorHandler(checkSchema(testimonialSchema)),
+    createTestimonial
+);
+
+module.exports = router;
