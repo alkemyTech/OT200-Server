198ba35a43e139e929a3dfe20117227923ba998f
gtiavo <victorg.avila@gmail.com>
2022-06-02 11:15:45 -0300

    fix: add associate-models/comment - models/new

--- models/comment.js
+++ models/comment.js
@@ -9,6 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Comment.belongsTo(models.New, {
+        as: 'new',
+        foreignKey: 'id',
+        target_key: 'post_id'
+      });
     
     }
   };
