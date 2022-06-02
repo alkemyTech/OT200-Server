641e70fef0f7828b8229ed4884690c5fa545e1da
Mario Martinez <mariomtz091@gmail.com>
2022-06-01 21:31:43 -0300

    Correccion en controller.login

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,22 +1,29 @@
 const bcrypt = require('bcrypt');
-const findUser = require('../services/auth');
+const {findUser} = require('../services/auth');
+const userToken = require('../middleware/userToken');
 
 const userController = {
     login: (req, res) => {
         try{
-            const {password} =  req.body;
+            const {password } = req.body;
             
-            const user = findUser(req.body.email);      
-          
-            if(!user)res.status(404).json({message: 'Usuario no encontrado', ok: false});
+            const user = findUser(req.body);
 
-            let isValidPassword = bcrypt.compareSync(password, user.password);
+            if(!user){
+                return res.status(400).json({message: 'Usuario no encontrado', ok: false});
+            }
 
-            if(!isValidPassword) res.status(401).json({message: 'Contraseña incorrecta', ok: false});
-            
+            const isValidPassword = bcrypt.compareSync(password, user.password);
 
-            res.status(200).json({message: 'Login exitoso', user: user});
+            if(!isValidPassword) res.status(401).json({message: 'Contraseña incorrecta', ok: false})
 
+            const token = userToken.generateToken(user);
+
+            return res.status(200).json({
+                message: 'Login exitoso',
+                token
+
+            });
         }catch(error){
             return res.status(500).json({
                 message: 'Error',
