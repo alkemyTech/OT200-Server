7c3d1f3a01d27393cdb0fa897a962a3d49280e97
gtiavo <victorg.avila@gmail.com>
2022-06-03 20:29:35 -0300

    add endpoint get - routes/users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const { getAllUsers } = require('../controllers/users.controller');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.get('/', getAllUsers )
 
 module.exports = router;
