efc762bc4bec65eac1ae013fc0571bd9a53ca4cc
Robert Santos <rssp_96@hotmail.com>
2022-06-03 23:59:55 -0500

    controller fixed

--- controllers/members.controller.js
+++ controllers/members.controller.js
@@ -2,24 +2,17 @@ const { request, response } = require("express");
 const db = require("../models");
 const Member = db.member;
 
+
 const getMembers = (req = request, res = response) => {
   res.status(200).json({ msg: "getMembers" });
 };
 
 const createMember = async (req, res) => {
-  const { name } = req.body;
-  if (!name || name === undefined || typeof name !== "string") {
-    return res.json({
-      success: false,
-      message: "Please introduce a valid input",
-    });
-  } else {
-    try {
-      const newMember = await Member.create(req.body);
-      return res.status(200).json(newMember);
-    } catch (error) {
-      return res.status(404).json(error.message);
-    }
+  try {
+    const newMember = await Member.create(req.body);
+    return res.status(201).json(newMember);
+  } catch (error) {
+    return res.status(500).json(error.message);
   }
 };
 
