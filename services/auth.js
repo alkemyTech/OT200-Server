b185263e14c68d9d4b9bdf7341ddb9eefb78c998
Mario Martinez <mariomtz091@gmail.com>
2022-06-06 04:04:37 -0300

    Se agrego error a auth services

--- services/auth.js
+++ services/auth.js
@@ -4,6 +4,10 @@ const findUser = async (email) => {
 
     const user = await db.User.findOne({ where: { email } });
 
+    if (!user) {
+        throw new Error('No se encontró el usuario');
+    }
+    
     return user
 }
 
