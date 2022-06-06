828f530d9e9b2993196353f951746c5c4a520d39
gtiavo <victorg.avila@gmail.com>
2022-06-06 20:11:26 -0300

    add try-cach 'getAllUsers' controllers/user.controllers

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,10 +3,19 @@ const { findAll } = require('../services/user');
 //Controllers:
 const getAllUsers = async(req, res) => {
 
+  try {
+
     const users = await findAll();
 
-    res.status(200).json({msg:'ok', users});
+    res.status(200).json({error: false, message:'ok', users});
+    
+  } catch (error) {
+
+    console.log(error.stack);
+    res.status(500).json({error: true, message:'Error en el servidor, comuniquese con el administrador', users:null});
+    
   }
+  };
 
 
 
