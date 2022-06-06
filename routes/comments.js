0dc22c3380a554c9f7301db9ca712780420938a9
gtiavo <victorg.avila@gmail.com>
2022-06-06 19:15:22 -0300

    correciones en el endpoint routes/comments

--- routes/comments.js
+++ routes/comments.js
@@ -1,22 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const { check } = require('express-validator');
 
-const validatorHandler = require('../middlewares/validatorHandler');
+const verifyToken = require('../middleware/verifyToken');
+const validatorHandler = require('../middleware/validatorHandler');
+const commentsFields = require('../helpers/checkCommentsFields');
+const { newComment } = require('../controllers/comments.controller');
 
 
-router.post('/',
-
-validatorHandler([
-    check('news_id', 'El campo news_id es obligatorio').not().isEmpty(),
-    check('user_id', 'El campo user_id es obligatorio').not().isEmpty(),
-    check('body', 'El campo body es obligatorio').trim().not().isEmpty(),
-]),
-
-(req, res ) => {
-    const { news_id, user_id, body } = req.body;
-    res.json({msg: 'Crear comentario', news_id, user_id, body})
-});
+router.post('/', verifyToken, validatorHandler(commentsFields), newComment );
 
 
 
