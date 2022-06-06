171a2b1beda1246dc5da7ef8a431e13b7af1fb5a
gtiavo <victorg.avila@gmail.com>
2022-06-06 12:09:29 -0300

    fix: agregue control de errores - controllers/categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -5,11 +5,18 @@ class CategoryController {
  static async newCategory(req, res){
       const { name, description, image } = req.body;
       
+      try {
+            const categoria = await createCategory({ name, description, image });
       
-      const categoria = await createCategory({ name, description, image });
+      
+            res.status(200).json({ error: false, message:'La categoría se creo con exito', category: categoria});
+            
+      } catch (error) {
 
-
-      res.status(200).json({ msg:'ok', categoria});
+            console.log(error);
+            res.status(500).json({ error: true, message:'Ah ocurrido un error, comuniquese con administración', category: null});
+      }
+      
 }
 
   
