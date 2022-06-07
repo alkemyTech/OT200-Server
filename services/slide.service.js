f0f82403b48c78fa0a01d1ca6ef2558c87ea2508
gtiavo <victorg.avila@gmail.com>
2022-06-07 12:29:35 -0300

    add message error 'destroySlide' - services/slide.service

--- services/slide.service.js
+++ services/slide.service.js
@@ -10,15 +10,16 @@ const createSlide = async (slide) => {
 }
 
 
-//Elimina un registro de la DB
+
 const destroySlide = async( id ) => {
-    //Busca el registro en la Db por id
+    
+    
     const slide = await slideModel.findByPk(id);
 
-    //Si el registro no existe, devuelve un mensaje de error.
-    if( !slide ) throw  `El slide con id: -${id}- no se encuentra en DB`;
+    
+    if( !slide ) throw {message: `El slide con id: -${id}- no se encuentra en DB`, status:400};
 
-    //Si el regitro existe lo elimina.
+    
     await slide.destroy();
 
     return;
