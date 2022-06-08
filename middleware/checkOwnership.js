71bd7dd5d1e5d43cd3895c1ed3764eeec1b5aa47
gabriel <gamcode98@gmail.com>
2022-06-08 19:08:35 -0300

    changes added

--- middleware/checkOwnership.js
+++ middleware/checkOwnership.js
@@ -2,10 +2,11 @@ const checkOwnership = (req, res, next) => {
 
     const { id } = req.params;
     const tokenId = req.user.id;
+    const tokenRoleId = req.user.roleId
 
-    if (Number(id) === tokenId || tokenId === 1) return next();
+    if (Number(id) === tokenId || tokenRoleId === 1) return next();
 
-    return res.status(403).json({
+    return res.status(401).json({
         error: true,
         message: "Insufficient permissions",
     });
