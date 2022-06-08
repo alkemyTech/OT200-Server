52f0904a241a78aefc966061fe37429b3dd0863b
Robert Santos <rssp_96@hotmail.com>
2022-06-08 00:28:30 -0500

    conflicts

--- routes/members.js
+++ routes/members.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const router = express.Router();
+const { getMembers } = require("../controllers/members.controller");
+const checkAdmin = require("../middleware/checkAdmin");
+const verifyToken = require("../middleware/verifyToken");
 const { createMember } = require("../controllers/members.controller");
+
+router.get(
+  "/",
+  // verifyToken,
+  // checkAdmin,
+  getMembers
+);
+
 const {
   verifyInputs,
   errorhandler,
 } = require("../middleware/verifyMembersInputs");
 const verifyToken = require("../middleware/verifyToken");
 
-const router = express.Router();
 
 router.post("/", verifyToken, verifyInputs, errorhandler, createMember);
 
