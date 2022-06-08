089ff6b79fe55f4864613f77b46bd55389bf6bbc
gtiavo <victorg.avila@gmail.com>
2022-06-07 22:02:53 -0300

    fix: delete comments routes/users

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,7 @@ var router = express.Router();
 const { checkAdmin, verifyToken } = require('../middleware');
 const { getAllUsers } = require('../controllers/users.controller');
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+
 
 router.get('/', verifyToken, checkAdmin, getAllUsers );
 
