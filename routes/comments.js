0ba9953d5879a56fd8a73023cf4fc3f1550a065c
Robert Santos <rssp_96@hotmail.com>
2022-06-09 17:18:05 -0500

    endpoint

--- routes/comments.js
+++ routes/comments.js
@@ -3,11 +3,12 @@ const router = Router();
 
 const verifyToken = require('../middleware/verifyToken');
 const validatorHandler = require('../middleware/validatorHandler');
-const commentsFields = require('../helpers/checkCommentsFields');
+const { commentsFields, updateFields } = require("../helpers/checkCommentsFields");
 const { newComment } = require('../controllers/comments.controller');
 
 
-router.post('/', verifyToken, validatorHandler(commentsFields), newComment );
+router.post('/', verifyToken, validatorHandler(commentsFields), newComment);
+router.put("/:id", verifyToken, validatorHandler(updateFields), verifyOwner, updateComment);
 
 
 
