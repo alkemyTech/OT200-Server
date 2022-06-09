ce2e26ddd98d0d24aab83346e8b706d7ecaa3b88
Mario Martinez <mariomtz091@gmail.com>
2022-06-09 12:10:28 -0300

    Correcion en services/news

--- services/news.js
+++ services/news.js
@@ -3,11 +3,7 @@ const db = require('../models/index');
 const findId = async (id) => {
     
     const news = await db.News.findByPk(id);
-
-    if(news === null){
-        console.log('No se encontró la noticia');
-    }
-
+    
     return news
 
 }
