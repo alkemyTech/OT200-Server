91048cad5d28bb40e1f073fce9c90081f006e4df
gtiavo <victorg.avila@gmail.com>
2022-06-08 23:29:00 -0300

    add route 'categories/:id' - routes/categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const getCategories = require('../controllers/categories').getAllCategories;
+const  { getAllCategories, newCategory, getOneCategory } = require('../controllers/categories');
 const verifyToken = require('../middleware/verifyToken');
 const checkAdmin = require('../middleware/checkAdmin');
 
 const { categoriesFields } = require('../helpers');
 const validatorHandler = require('../middleware/validatorHandler');
-const { newCategory } = require('../controllers/categories');
+
 
 
 router.post('/',verifyToken , checkAdmin, validatorHandler( categoriesFields ), newCategory);
 
+router.get("/", verifyToken, checkAdmin, getAllCategories);
 
-router.get("/", verifyToken, checkAdmin, getCategories);
+router.get("/:id", verifyToken, checkAdmin, getOneCategory);
 
 
 
