66108c9a9ea0aba3384edb07f90add41096f95e4
Mario Martinez <mariomtz091@gmail.com>
2022-06-09 12:50:19 -0300

    integracion de template de welcomeMail

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,17 @@
 const create = require("../services/user");
 
+// const subject = "Bienvenidos a la ONG Somos Más";
+// const emailText = "Hola, te damos la bienvenida a la ONG Somos Más, esperamos que disfrutes de nuestros servicios.";
+// const contactInfo = "Para contactar con nosotros, puedes enviarnos un correo a: somosfundacionmas@gmail.com, Instagram: @somosfundacionmas, Facebook: Somos_Más, o por teléfono: 1160112988";
+
+
 const createUser = async (req, res) => {
     try {
 
         const data = req.body;
         const newUser = await create(data)
+
+        // en la funcion welcomeMail agregar los parametros emailTitle=data.firstName, emailText, contactInfo, email y subject.
         
         return res.status(201).json(newUser);
 
---
+++ services/sendMail.js
@@ -0,0 +1,37 @@
+const sgMail = require('@sendgrid/mail');
+const ejs = require('ejs');
+const path = require('path');
+
+const welcomeMail = async (emailTitle, emailText, contactInfo, email, subject) => {
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    let templateEmail = await ejs.renderFile(path.join(__dirname,'../views/templateEmail.ejs'), 
+    {
+        emailTitle: emailTitle,
+        emailText: emailText,
+        contactInfo: contactInfo
+    })
+    
+    const msg = {
+      to: email,
+      from: process.env.SENGRID_MAIL,
+      subject: subject,     
+      html: templateEmail,
+    };
+    
+    //ES8
+    (async () => {
+      try {
+        await sgMail.send(msg);
+      } catch (error) {
+        console.error(error);
+    
+        if (error.response) {
+          console.error(error.response.body)
+        }
+      }
+    })();
+}
+
+module.exports = welcomeMail;
