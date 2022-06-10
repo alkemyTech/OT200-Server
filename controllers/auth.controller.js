97ef00aba423a3d6f64a5b283429ca3926527e15
Mario Martinez <mariomtz091@gmail.com>
2022-06-10 17:09:21 -0300

    test envio de mail

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,14 +2,28 @@ const {create} = require("../services/user");
 const findUser = require('../services/auth');
 const bcrypt = require('bcrypt');
 const userToken = require('../middleware/userToken');
+const {welcomeMail} = require('../services/sendMail');
+
+const subject = "Bienvenidos a la ONG Somos Más";
+const emailText = "Hola, te damos la bienvenida a la ONG Somos Más, esperamos que disfrutes de nuestros servicios.";
+const contactInfo = "Para contactar con nosotros, puedes enviarnos un correo a: somosfundacionmas@gmail.com, Instagram: @somosfundacionmas, Facebook: Somos_Más, o por teléfono: 1160112988";
+
 
 const createUser = async (req, res) => {
     try {
 
         const data = req.body;
         const newUser = await create(data)
-        
-        return res.status(201).json(newUser);
+
+        const emailTitle = `Bienvenido ${newUser.name}`;
+
+        if(newUser){
+            welcomeMail(subject, emailText, contactInfo, newUser.email, emailTitle)
+            return res.status(201).json({
+                message: 'Usuario creado',
+                newUser
+            });
+        }
 
     } catch (error) {
 
