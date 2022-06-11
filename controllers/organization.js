48467d6dd77a78baa308c26bcd1b657ff0a7a611
gtiavo <victorg.avila@gmail.com>
2022-06-11 12:29:23 -0300

    require "updatedPublicData" - controllers/organization

--- controllers/organization.js
+++ controllers/organization.js
@@ -1,7 +1,35 @@
+const { updatedPublicData } = require('../services/organization');
 
+const updatedOrganizationData = async(req, res) => {
 
-const updatedOrganizationData = (req, res) => {
-    res.status(200).json({ message: 'Datos publicos'})
+    const { body, params } = req;
+    const { name, image, email, welcomeText, instagramUrl, facebookUrl, linkedinUrl } = body;
+
+    try {
+        
+        const newData = await updatedPublicData(params.id, { 
+            name, 
+            image, 
+            email, 
+            welcomeText, 
+            instagramUrl, 
+            facebookUrl, 
+            linkedinUrl 
+        });
+    
+        res.status(200).json({error: false, message: 'ok', data:newData});
+        
+    } catch (error) {
+
+        if( !error.status ) {
+
+            return res.status(500).json( {error: true, message: 'Error en el servidor: ' + error.message} );
+    
+          }
+    
+        res.status(error.status).json( {error: true, message: error.message} );
+    }
+
 };
 
 
