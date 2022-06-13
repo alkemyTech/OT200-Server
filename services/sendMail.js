0c7286371eb19047506119dd2b0f7a60bbe3653a
Mario Martinez <mariomtz091@gmail.com>
2022-06-13 13:02:04 -0300

    correccion de ticket con template

--- services/sendMail.js
+++ services/sendMail.js
@@ -1,20 +1,11 @@
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
-const ejs = require('ejs');
-const path = require('path');
 
 
-const welcomeMail = async (emailTitle, emailText, contactInfo, email, subject) => {
+const welcomeMail = async ( email, subject) => {
 
     sgMail.setApiKey(process.env.SENGRID_API_KEY);
 
-
-    let templateEmail = await ejs.renderFile(path.join(__dirname,'../views/templateEmail.ejs'), 
-    {
-        emailTitle: emailTitle,
-        emailText: emailText,
-        contactInfo: contactInfo
-    })
 
     const msg = {
       to: email,
