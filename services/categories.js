4819f11055fa73e914e04c5c4a395f68f27584e0
gtiavo <victorg.avila@gmail.com>
2022-06-13 20:45:32 -0300

    add function "categoryList" - services/categories

--- services/categories.js
+++ services/categories.js
@@ -24,9 +24,37 @@ const createCategory = async(data) => {
 };
 
 
+const categoryList = async( data ) => {
+
+    const page = data - 1;
+    const limite = 10;
+    const next = (limite + data);
+    const previous = ( data - limite );
+
+    const { count, rows  } = await db.Categories.findAndCountAll({ offset: page, limit: limite });
+
+    if( data > count ) throw { message:`La cantidad de registros es de: ${ count }`, status:400 };
+
+    const categories = {
+        count,
+        limit:10,
+        rows,
+        nextPage: next > count ? null : `${ process.env.HTTP_DEVELOP }/categories?page=${ next }`,
+        currentPage:  data === 1 ? null 
+                    : previous > next ? null 
+                    : previous <= 0 ? `${ process.env.HTTP_DEVELOP }/categories?page=1` 
+                    : `${ process.env.HTTP_DEVELOP }/categories?page=${ previous }`
+       
+    }
+
+    return categories;
+};
+
+
 
 module.exports = {
     findAll,
     createCategory,
-    deleteOne
+    deleteOne,
+    categoryList,
 };
