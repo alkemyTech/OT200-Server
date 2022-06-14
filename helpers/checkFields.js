ac3acd7f610ec3c01f4c6d6ad78dd71f11eaed39
gtiavo <victorg.avila@gmail.com>
2022-06-14 12:14:26 -0300

    add organizationFields - helpers/checkFields

--- helpers/checkFields.js
+++ helpers/checkFields.js
@@ -5,9 +5,19 @@ const categoriesFields = [
     check('name', 'El campo name es obligatorio').trim().not().isEmpty().isString(),
 ];
 
-
+const organizationFields = [
+    check('name', 'El campo name es obligatorio').trim().not().isEmpty().isString(),
+    check('image', 'El campo image es obligatorio').trim().not().isEmpty().isString(),
+    check('email', 'El campo email es obligatorio').not().isEmpty(),
+    check('email', 'Tiene que ser un formato de email valido').isEmail(),
+    check('welcomeText', 'El campo welcomeText es obligatorio').trim().not().isEmpty().isString(),
+    check('facebookUrl', 'El campo facebookUrl es obligatorio').trim().not().isEmpty().isString(),
+    check('instagramUrl', 'El campo instagramUrl es obligatorio').trim().not().isEmpty().isString(),
+    check('linkedinUrl', 'El campo linkedinUrl es obligatorio').trim().not().isEmpty().isString(),
+];
 
 
 module.exports = {
-    categoriesFields
+    categoriesFields,
+    organizationFields
 }
