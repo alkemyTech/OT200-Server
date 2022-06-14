871595c4e79a867bcf766d56215e8451d8642e5e
gtiavo <victorg.avila@gmail.com>
2022-06-14 12:17:15 -0300

    add admin permissions && validations fields - routes/organization

--- routes/organization.js
+++ routes/organization.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
+const { verifyToken, checkAdmin } = require('../middleware');
+const validatorHandler = require('../middleware/validatorHandler');
+const { organizationFields } = require('../helpers');
 const { updatedOrganizationData } = require('../controllers/organization');
 
 
-router.put('/public/:id', updatedOrganizationData )
+router.put('/public/:id', verifyToken, checkAdmin, validatorHandler(organizationFields), updatedOrganizationData );
 
 
 module.exports = router;
