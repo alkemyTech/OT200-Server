dc4163d041a1f7fc2a44a8f99fe6dc2175f42921
Mario Martinez <mariomtz091@gmail.com>
2022-06-15 01:25:17 -0300

    Typo en organization services

--- services/organization.js
+++ services/organization.js
@@ -2,7 +2,7 @@ const db = require('../models');
 
 const getPublicOrganizationService = async() => {
 
-    const publicOrganization = await db.Organization.findAll({attributes: ['name', 'image', '`phone`', 'address']});
+    const publicOrganization = await db.Organization.findAll({attributes: ['name', 'image', 'phone', 'address']});
 
     if(!publicOrganization) {
         throw new Error("Error al obtener la organización");
