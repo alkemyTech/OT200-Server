ce22c6891892cf99e236fec0540dd2ac0f303343
Robert Santos <rssp_96@hotmail.com>
2022-06-14 22:39:19 -0500

    changes

--- helpers/checkCommentsFields.js
+++ helpers/checkCommentsFields.js
@@ -1,18 +1,15 @@
-const { check } = require('express-validator');
-
+const { check } = require("express-validator");
 
 const commentsFields = [
-    check('post_id', 'El campo post_id es obligatorio').not().isEmpty(),
-    check('user_id', 'El campo user_id es obligatorio').not().isEmpty(),
-    check('body', 'El campo body es obligatorio').trim().not().isEmpty(),
+  check("post_id", "El campo post_id es obligatorio").not().isEmpty(),
+  check("user_id", "El campo user_id es obligatorio").not().isEmpty(),
+  check("body", "El campo body es obligatorio").trim().not().isEmpty(),
 ];
 
 const updateFields = [
-    check("user_id", "Please introduce a valid user_id").not().isEmpty(),
-    check('post_id', 'Please introduce a valid post_id').isEmpty(),
-    check('body', 'Please introduce a update data').not().isEmpty().isString(),
-]
-
-
+  check("user_id", "Please introduce a valid user_id").not().notEmpty(),
+  check("post_id", "Please introduce a valid post_id").notEmpty(),
+  check("body", "Please introduce a update data").notEmpty().isString(),
+];
 
 module.exports = { commentsFields, updateFields };
