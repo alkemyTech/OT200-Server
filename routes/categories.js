62a53c2336a840f2a12301d40f3c2544ee2236dd
Robert Santos <rssp_96@hotmail.com>
2022-06-14 22:03:14 -0500

    verifyInputs

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const getCategories = require('../controllers/categories').getAllCategories;
-const verifyToken = require('../middleware/verifyToken');
-const checkAdmin = require('../middleware/checkAdmin');
+const getCategories = require("../controllers/categories").getAllCategories;
+const verifyToken = require("../middleware/verifyToken");
+const checkAdmin = require("../middleware/checkAdmin");
 const { updateCategory } = require("../controllers/categories");
-
+const validatorHandler = require("../middleware/validatorHandler");
+const bodyrequest = require("../helpers/checkCategories");
 
 router.get("/", verifyToken, checkAdmin, getCategories);
-router.put("/:id",verifyToken,checkAdmin, updateCategory)
-
+router.put(
+  "/:id",
+  verifyToken,
+  checkAdmin,
+  validatorHandler(bodyrequest),
+  updateCategory
+);
 
 module.exports = router;
