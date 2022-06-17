dde52dedcc1617ab93b0f64fd62c29410d5da372
gtiavo <victorg.avila@gmail.com>
2022-06-17 18:55:35 -0300

    add test 'status:401', "GET/members" - tests/members.test

--- tests/members.test.js
+++ tests/members.test.js
@@ -5,17 +5,26 @@ const generateToken = require('../middleware/userToken');
 const user = { id: 1, name:'PruebaTest', roleId: 1 }
 const token = generateToken(user);
 
+const members = [];
 
 describe("GET/members", () => {
 test('Debera mostrar un listado con todos los miembros, status:200', (done) => {
-    const members = [];
      request(app)
     .get('/members')
-    .send(members)
     .set('x-access-token', token)
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
     .expect(200, done)
-    .expect([])
+    .expect(members)
  });
+
+ test('No se ah enviado el token de autenticación, status:401', (done) => {
+    request(app)
+    .get('/members')
+    .set('x-access-token', '')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(401, done)
+    .expect({ error: "No se ha enviado el token de autenticación"})
+  });
 });
