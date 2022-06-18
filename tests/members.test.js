80d3041c8ef5323722517e6ff489eee844055eff
gtiavo <victorg.avila@gmail.com>
2022-06-18 12:13:26 -0300

    add test 'POST/members'

--- tests/members.test.js
+++ tests/members.test.js
@@ -4,8 +4,82 @@ const generateToken = require('../middleware/userToken');
 
 const user = { id: 1, name:'PruebaTest', roleId: 1 }
 let token = generateToken(user);
+const tokenNull = '12345';
 
-const members = [];
+
+
+
+describe('POST/members', () => {
+
+  test('Debera crear un member, status: 201', ( done ) => {
+
+      request(app)
+      .post('/members')
+      .set('x-access-token', token)
+      .send({name:'pruebaTest'})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, res) =>  {
+          if (err) return done(err);
+          return done();
+        });
+
+   });
+
+
+  test('Debera devolver un status 400 si no se envian los campos por request', ( done ) => { 
+
+      request(app)
+      .post('/members')
+      .set('x-access-token', token)
+      .send({name:''})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end((err, res) =>  {
+          if (err) return done(err);
+          return done();
+        });
+
+   });
+
+  test('No se ah enviado el token de autenticación, status:401', (done) => {
+
+      request(app)
+      .post('/members')
+      .set('x-access-token', '')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .expect({ error: "No se ha enviado el token de autenticación"})
+      .end((err, res) =>  {
+        if (err) return done(err);
+        return done();
+      });
+
+  });
+
+  test('Token invalido, status:400', (done) => { 
+
+
+      request(app)
+      .post('/members')
+      .set('x-access-token', tokenNull )
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .expect({ message: "Token invalido" })
+      .end((err, res) =>  {
+        if (err) return done(err);
+        return done();
+      });
+
+  });
+
+});
+
+
 
 describe("GET/members", () => {
 
@@ -16,8 +90,12 @@ test('Debera mostrar un listado con todos los miembros, status:200', (done) => {
     .set('x-access-token', token)
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect(200, done)
-    .expect(members)
+    .expect(200)
+    .end((err, res) =>  {
+      if (err) return done(err);
+      return done();
+    });
+    
 
  });
 
@@ -28,38 +106,52 @@ test('Debera mostrar un listado con todos los miembros, status:200', (done) => {
     .set('x-access-token', '')
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect(401, done)
+    .expect(401)
     .expect({ error: "No se ha enviado el token de autenticación"})
+    .end((err, res) =>  {
+      if (err) return done(err);
+      return done();
+    });
 
   });
 
   test('Token invalido, status:400', (done) => { 
 
-    token = '12345'
 
     request(app)
     .get('/members')
-    .set('x-access-token', token )
+    .set('x-access-token', tokenNull )
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect(400, done)
+    .expect(400)
     .expect({ message: "Token invalido" })
-    
+    .end((err, res) =>  {
+      if (err) return done(err);
+      return done();
+    });
+
   });
 
   test('No tiene los permisos de adminitrador, status:403', ( done ) => { 
 
     user.roleId = 2;
-    const token = generateToken(user);
+    const tokenId2 = generateToken(user);
 
     request(app)
     .get('/members')
-    .set('x-access-token', token )
+    .set('x-access-token', tokenId2 )
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
-    .expect(403, done)
+    .expect(403)
     .expect({  msg: 'No tiene los permisos necesarios para acceder a esta información, comuniquese con admisntración'})
+    .end((err, res) =>  {
+      if (err) return done(err);
+      return done();
+    });
 
    });
 
 });
+
+
+
