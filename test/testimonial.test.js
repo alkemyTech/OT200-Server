baa463981338ec9698c6b10de1e43052cb857f67
gabriel <gamcode98@gmail.com>
2022-06-22 18:18:13 -0300

    new tests were added

--- test/testimonial.test.js
+++ test/testimonial.test.js
@@ -1,116 +1,193 @@
 const supertest = require("supertest");
+const generateToken = require("../middleware/userToken");
 const app = require("./../app");
 const db = require("./../models/");
 const api = supertest(app);
 
-const initialTestimonials = [
-  {
-    name: "Michi 1",
-    image: "Michi-1.jpg",
-    content: "Hello michi 1",
-  },
-  {
-    name: "Michi 2",
-    image: "Michi-2.jpg",
-    content: "Hello michi 2",
-  },
-];
+const adminToken = generateToken({ id: 1, roleId: 1 });
 
-const admin = {
-  email: "admin@gmail.com.ar",
-  password: "sdswA@ssds2",
-};
+const standardToken = generateToken({ id: 2, roleId: 2 });
 
-let token = "";
+const emptyToken = "";
 
-beforeEach(async () => {
-  await db.Testimonial.destroy({ where: {}, truncate: true, force: true });
+const invalidToken = "asd123";
 
-  initialTestimonials.forEach(async (testimonial) => {
-    await db.Testimonial.create(testimonial);
-  });
-
-  const response = await api.post("/auth/login").send(admin);
-  token = response.body.token;
-});
+beforeAll(async () => await db.Testimonial.destroy({ where: {}, truncate: true, force: true }));
 
 describe("POST /testimonials", () => {
-  describe("a valid testimonial can be added", () => {
-    const newTestimonial = {
-      name: "Michi test",
-      image: "Michi-test.jpg",
-      content: "Hello michi test",
-    };
 
-    test("should respond with a 201 status code", async () => {
+  const newTestimonial = {
+    name: "Jhon test",
+    image: "Jhon-test.jpg",
+    content: "Hello Jhon test",
+  };
+
+  describe("token is not send", () => {
+
+    test("should respond with a 401 status code", async () => {
+
       await api
         .post("/testimonials")
-        .set("x-access-token", token)
+        .set("x-access-token", emptyToken)
+        .expect(401)
+        .expect("Content-Type", /application\/json/);
+
+    });
+
+  });
+
+  describe("an invalid token is sent", () => {
+
+    test("should respond with a 400 status code", async () => {
+
+      await api
+        .post("/testimonials")
+        .set("x-access-token", invalidToken)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+
+    });
+
+  });
+
+  describe("a valid testimonial is added by an admin user", () => {
+
+    test("should respond with a 201 status code", async () => {
+
+      await api
+        .post("/testimonials")
+        .set("x-access-token", adminToken)
         .send(newTestimonial)
         .expect(201)
         .expect("Content-Type", /application\/json/);
 
-      const response = await api
-        .get("/testimonials")
-        .set("x-access-token", token)
-        .expect(200)
-        .expect("Content-Type", /application\/json/);
-
-      expect(response.body).toHaveLength(initialTestimonials.length + 1);
     });
+
   });
 
-  describe("a testimonial without name (required) is not added", () => {
+  describe("a testimonial without name (required) is not added by an admin user", () => {
+    
     const newTestimonial = {
-      image: "Michi-test.jpg",
-      content: "Hello michi test",
+      image: "Jhon-test.jpg",
+      content: "Hello Jhon test",
     };
 
     test("should respond with a 400 status code", async () => {
+
       await api
         .post("/testimonials")
-        .set("x-access-token", token)
+        .set("x-access-token", adminToken)
         .send(newTestimonial)
         .expect(400)
         .expect("Content-Type", /application\/json/);
+
     });
+
   });
+
+  describe("a new testimonial is not added by a standard user", () => {
+    
+    test("should respond with a 403 status code", async () => {
+
+      await api
+        .post("/testimonials")
+        .set("x-access-token", standardToken)
+        .send(newTestimonial)
+        .expect(403)
+        .expect("Content-Type",/application\/json/);
+
+    })
+    
+  });
+
 });
 
 describe("DELETE /testimonials", () => {
-  describe("a testimonial deleted by id", () => {
-    test("should respond with a 200 status code", async () => {
+
+  describe("token is not send", () => {
+
+    test("should respond with a 401 status code", async () => {
+
       await api
         .delete(`/testimonials/${1}`)
-        .set("x-access-token", token)
+        .set("x-access-token", emptyToken)
+        .expect(401)
+        .expect("Content-Type", /application\/json/);
+
+    });
+
+  });
+
+  describe("an invalid token is sent", () => {
+
+    test("should respond with a 400 status code", async () => {
+
+      await api
+        .delete(`/testimonials/${1}`)
+        .set("x-access-token", invalidToken)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+
+    });
+
+  });
+
+  describe("a testimonial deleted by id", () => {
+
+    test("should respond with a 200 status code", async () => {
+
+      await api
+        .delete(`/testimonials/${1}`)
+        .set("x-access-token", adminToken)
         .expect(200)
         .expect("Content-Type", /application\/json/);
+
     });
+
   });
 
   describe("a testimonial not found", () => {
+
     test("should respond with a 404 status code", async () => {
+
       await api
         .delete(`/testimonials/${123}`)
-        .set("x-access-token", token)
+        .set("x-access-token", adminToken)
         .expect(404)
         .expect("Content-Type", /application\/json/);
+
     });
+
   });
 
-  describe("an id invalid", () => {
+  describe("an id invalid is sent", () => {
+
     test("should respond with a 500 status code", async () => {
+
       await api
         .delete(`/testimonials/${"asdsad"}`)
-        .set("x-access-token", token)
+        .set("x-access-token", adminToken)
         .expect(500)
         .expect("Content-Type", /application\/json/);
+
     });
+
   });
+
+  describe("a testimonial is not deleted by a standard user", () => {
+    
+    test("should respond with a 403 status code", async () => {
+
+      await api
+        .delete(`/testimonials/${1}`)
+        .set("x-access-token", standardToken)        
+        .expect(403)
+        .expect("Content-Type",/application\/json/);
+
+    })
+    
+  });
+
 });
 
-afterAll(async () => {
-  //* TODO: cerrar tambien la base de datos
-  // server.close()
-  await db.Testimonial.destroy({ where: {}, truncate: true, force: true });
-});
+afterAll(async () => await db.Testimonial.destroy({ where: {}, truncate: true, force: true }));
