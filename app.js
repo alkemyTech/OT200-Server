7c8498ffa52ace20246ff1e2654bedf17e33260c
Mario Martinez <mariomtz091@gmail.com>
2022-06-23 01:39:57 -0300

    Se creo documentacion y config de news

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
 require('dotenv').config()
+const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const options = require('./doc/swaggerOption')
+
 
 const indexRouter = require('./routes/index');
 
@@ -21,7 +25,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const specsNews = swaggerJsDoc(options)  
+
 app.use('/', indexRouter);
+app.use('/news-docs', swaggerUi.serve, swaggerUi.setup(specsNews))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
