580e81b3079b8b71b0c885cabb9f75b940c4156e
Mario Martinez <mariomtz091@gmail.com>
2022-06-24 02:11:15 -0300

    test de news

--- tests/news.test.js
+++ tests/news.test.js
@@ -12,68 +12,87 @@ const news = {
 }
 
 const user = {
-    firstName: 'Test user',
-    lastName: 'Test user',    
-    email: 'test@test.com',
-    password: 'test1234!',
+    id: 1,
     roleId: 1,
+}
+const userPublic = {
+    id:2,
+    roleId: 2,
+
 }
 
 const token = generateToken(user)
+const tokenPublic = generateToken(userPublic)
 
-beforeAll(async () => {
+beforeEach(async () => {
 
     await db.News.destroy({where:{} ,truncate: true, cascade: true })
 
 })
 
 
-describe('POST /news', () => {
+// describe('GET /news/:id', () => {
 
-    describe('Crea una noticia', () => {
+//     describe('Muestra una noticia',  () => {
+                
+//     test('Deberia mostrar una noticia', async () => {
+//         await request(app)
+//         .get(`/news/${1}`)
+//         .set('Accept', /application\/json/)
+//         .expect(200)
+//     })
+// })
 
-        test('Deberia crear una noticia', (done) => {
-            request(app)
-            .post('/news')
-            .set('x-access-token', token)
-            .send(news)
-            .expect(201)
-            .expect('Content-Type', /json/)
-            .end(done)
-        })
-    })
+//       describe('Noticia inexistente', () => {
+//         const newsError = {
+//             id: 0
+//         }
+    
 
-    describe('Error por no ser Administrador', () => {
+//       test('Error por id inexistente', async () => {
+//         await request(app)
+//           .get(`/news/${0}`)
+//           .set('x-access-token', tokenPublic)
+//           .send(newsError)
+//           .expect(404)
+//           .expect('Content-Type', /application\/json/)
+//       })
+//   })
+//  })
 
-        test('Deberia mostrar error por no ser Administrador', (done) => {
-            request(app)
-            .post('/news')
-            .send(news)
-            .expect(401)
-            .expect('Content-Type', /json/)
-            .end(done)
-        })
-    })
 
-    describe('Error por no cumplir con las validaciones', () => {
+// describe('POST /news', () => {
 
-        const news = {
-            name: '',
-            content: '',
-            image: ''
-        }
+//     describe('Crea una noticia', () => {
 
-        test('Deberia mostrar error por no cumplir con las validaciones', (done) => {
-            request(app)
-            .post('/news')
-            .set('x-access-token', token)
-            .send(news)
-            .expect(400)
-            .expect('Content-Type', /json/)
-            .end(done)
-        })
-    })
-})
+//         test('Deberia crear una noticia', async () => {
+//             await request(app)
+//             .post('/news')
+//             .set('x-access-token', token)
+//             .send(news)
+//             .expect(201)
+//             .expect('Content-Type', /json/)            
+//         })
+//     })
+
+//     describe('Error por no cumplir con las validaciones', () => {
+
+//         const news = {
+//             name: '',
+//             content: '',
+//             image: ''
+//         }
+
+//         test('Deberia mostrar error por no cumplir con las validaciones', async () => {
+//             await request(app)
+//             .post('/news')
+//             .set('x-access-token', token)
+//             .send(news)
+//             .expect(400)
+//             .expect('Content-Type', /json/)            
+//         })
+//     })
+// })
 
 // describe('GET /news', () => {
 
@@ -82,6 +101,7 @@ describe('POST /news', () => {
 //         test('Deberia mostrar todas las noticias', () => {
 //         request(app)
 //             .get('/news')
+//               .set('x-access-token', tokenPublic) 
 //             .send(news)
 //             .expect(200)
 //             .expect('Content-Type', /json/)        
@@ -102,95 +122,58 @@ describe('POST /news', () => {
 // })
 
 
-describe('GET /news/:id', () => {
-
-    describe('Muestra una noticia siendo Usuario',  () => {
-        
-    test('Deberia mostrar una noticia', (done) => {
-        request(app)
-        .get(`/news/${news.id}`)
-        .send(news)
-        .expect(401)
-        .expect('Content-Type', /json/)
-        .end(done)
-    })
-})
-
-    describe('Noticia sin title', () => {
-        const newsError = {
-            name: '',
-            image: 'imageTest.jpg',
-            content: 'Test news content'
+describe('PUT /news/:id', () => {
+    
+    describe('Actualiza una noticia', () => {
+        const newsUpdate = {
+            id: 1,
+            name: 'New title',
+            content: 'New content',
+            image: 'newImageTest.jpg'
         }
-    
 
-    test('Error por no cumplir con las validaciones', (done) => {
-        request(app)
-        .get(`/news/${news.id}`)
-        .send(newsError)
-        .expect(401)
-        .expect('Content-Type', /json/)
-        .end(done)
+        test('Deberia actualizar una noticia', async () => {
+            await request(app)
+            .put(`/news/${1}`)
+            .set('x-access-token', token)
+            .send(newsUpdate)
+            .expect(200)
+            .expect('Content-Type', /json/)                        
     })
 })
-})
 
-
-
-// describe('PUT /news/:id', () => {
-    
-//     describe('Actualiza una noticia', () => {
-//         const news = {
-//             id: 1,
-//             name: 'New title',
-//             content: 'New content',
-//             image: 'newImageTest.jpg'
-//         }
-
-//         test('Deberia actualizar una noticia', (done) => {
-//             request(app)
-//             .put(`/news/${news.id}`)
-//             .set('x-access-token', token)
-//             .send(news)
-//             .expect(200)
-//             .expect('Content-Type', /json/)
-//             .end(done)            
-//     })
-// })
-
-//     describe('Error por no ser Administrador', () => {
+    describe('Error por no ser Administrador', () => {
             
-//             test('Deberia mostrar error por no ser Administrador', (done) => {
-//                 request(app)
-//                 .put('/news/${news.id}`')
-//                 .send(news)
-//                 .expect(401)
-//                 .expect('Content-Type', /json/)
-//                 .end(done)
-//             }
-//         )
-//     }
-//     )
+            test('Deberia mostrar error por no ser Administrador', async () => {
+                await request(app)
+                .put(`/news/${1}`)                
+                .set('x-access-token', '')
+                .send(news)
+                .expect(401)
+                .expect('Content-Type', /json/)                
+            }
+        )
+    }
+    )
 
-//     describe('Error por no encontrar la noticia', () => {
-//         const news = {
-//             id: '',
-//             title: 'New title',
-//             content: 'New content',
-//             image: 'newImageTest.jpg'
-//         }
+    describe('Error por no encontrar la noticia', () => {
+        const news = {
+            id: '',
+            title: 'New title',
+            content: 'New content',
+            image: 'newImageTest.jpg'
+        }
 
-//         test('Deberia mostrar error por no encontrar la noticia', (done) => {
-//             request(app)
-//             .put(`/news/${news.id}`)
-//             .set('x-access-token', token)
-//             .send(news)
-//             .expect(404)
-//             .expect('Content-Type', /json/)
-//             .end(done)
-//         })
-//     })
-// })
+        test('Deberia mostrar error por no encontrar la noticia', async () => {
+            await request(app)
+            .put(`/news/${news.id}`)
+            .set('x-access-token', token)
+            .send(news)
+            .expect(404)
+            .expect('Content-Type', "text/html; charset=utf-8")            
+        })
+    })
+})
 
 // describe('DELETE /news/:id', () => {
 
@@ -199,13 +182,12 @@ describe('GET /news/:id', () => {
 //             id: 1
 //         }
     
-//     test('Deberia eliminar una noticia', (done) => {
-//         request(app)
+//     test('Deberia eliminar una noticia', async () => {
+//         await request(app)
 //         .delete(`/news/${news.id}`)
 //         .set('x-access-token', token)
 //         .expect(200)
-//         .expect('Content-Type', /json/)
-//         .end(done)
+//         .expect('Content-Type', /json/)         
 //     })
 //     })
 
@@ -214,25 +196,23 @@ describe('GET /news/:id', () => {
 //             id: ''
 //         }
 
-//         test('Deberia mostrar error por no encontrar la noticia', (done) => {
-//             request(app)
+//         test('Deberia mostrar error por no encontrar la noticia', async () => {
+//            await request(app)
 //             .delete(`/news/${news.id}`)
 //             .set('x-access-token', token)
 //             .expect(404)
-//             .expect('Content-Type', /json/)
-//             .end(done)
+//             .expect('Content-Type', /json/)            
 //         })
 //     })
 
 //     describe('Error por no ser Administrador', () => {
 
-//         test('Deberia mostrar error por no ser Administrador', (done) => {
-//             request(app)
+//         test('Deberia mostrar error por no ser Administrador', async () => {
+//            await request(app)
 //             .delete('/news/:id')
 //             .send(news)
 //             .expect(401)
-//             .expect('Content-Type', /json/)
-//             .end(done)
+//             .expect('Content-Type', /json/)            
 //         })
 //     })
 
@@ -240,5 +220,4 @@ describe('GET /news/:id', () => {
 
 afterAll(async () => {
     await db.News.destroy({where: {} ,truncate: true, cascade: true })
-    await db.sequelize.close()
 })
