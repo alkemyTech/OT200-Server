bc8ac726bd6645b7aaf4cedc555f904ce24f5989
Mario Martinez <mariomtz091@gmail.com>
2022-06-25 00:41:14 -0300

    testeando get

--- tests/news.test.js
+++ tests/news.test.js
@@ -31,235 +31,242 @@ beforeEach(async () => {
 })
 
 
-describe('GET /news/:id', () => {
+// describe('GET /news/:id', () => {
 
-    describe('Muestra una noticia',  () => {
+//     describe('Muestra una noticia',  () => {
 
-    const id = 1
+//     const id = 1
                 
-    test('Deberia mostrar una noticia', async () => {
-        await request(app)
-        .get(`/news/${id}`)        
-        .set('x-access-token', tokenPublic)
-        .set('Accept', /application\/json/)
-        .expect(200)
-        .expect(response => {
-            expect(response.body.message).toEqual('Noticia encontrada')
-        })
-    })
-})
-
-      describe('Noticia inexistente', () => {
-    
-      test('Error por id inexistente', async () => {
-        await request(app)
-          .get(`/news/${0}`)
-          .set('x-access-token', token)
-          .send(news)
-          .expect(404)
-          .expect('Content-Type', /application\/json/)
-          .expect(response => {
-            expect(response.body.message).toEqual('No se encontró la noticia')
-        })
-      })
-  })
- })
-
-
-describe('POST /news', () => {
-
-    describe('Crea una noticia', () => {
-
-        test('Deberia crear una noticia', async () => {
-            await request(app)
-            .post('/news')
-            .set('x-access-token', token)
-            .send(news)
-            .expect(201)
-            .expect('Content-Type', /application\/json/)
-            .expect(response => {
-                expect(response.body).toBeInstanceOf( Object );
-            })            
-        })
-    })
-
-    describe('Error por no cumplir con las validaciones', () => {
-
-        const news = {
-            name: '',
-            content: '',
-            image: ''
-        }
-
-        test('Deberia mostrar error por no cumplir con las validaciones', async () => {
-            await request(app)
-            .post('/news')
-            .set('x-access-token', token)
-            .send(news)
-            .expect(400)
-            .expect('Content-Type', /application\/json/)
-            .expect(response => {
-                expect(response.body).toEqual({
-                    "errors": [
-                        {
-                            "value": '',
-                            "msg": "Image is required",
-                            "param": "image",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Title is required",
-                            "param": "name",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Title must be between 3 and 15 characters long",
-                            "param": "name",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Content is required",
-                            "param": "content",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Content must be at least 10 characters long",
-                            "param": "content",
-                            "location": "body"
-                        },
-                      ]
-                })                
-            })             
-        })
-    })
-})
-
-// describe('GET /news', () => {
-
-//     describe('Muestra todas las noticias', () => {  
-
-//         test('Deberia mostrar todas las noticias', () => {
-//         request(app)
-//             .get('/news')
-//               .set('x-access-token', tokenPublic) 
-//             .send(news)
-//             .expect(200)
-//             .expect('Content-Type', /application\/json/)        
-//         }) 
-//     })
-
-//     describe('Error por no encontrar noticias',  () => {
-//         const news = '';
-    
-//         test('Deberia mostrar error por no encontrar ninguna noticia', () => {
-//             request(app)
-//             .get('/news')
-//             .send(news)
-//             .expect(404)
-//             .expect('Content-Type', /application\/json/)        
+//     test('Deberia mostrar una noticia', async () => {
+//         await request(app)
+//         .get(`/news/${id}`)        
+//         .set('x-access-token', tokenPublic)
+//         .set('Accept', /application\/json/)
+//         .expect(200)
+//         .expect(response => {
+//             expect(response.body.message).toEqual('Noticia encontrada')
 //         })
 //     })
 // })
 
-
-describe('PUT /news/:id', () => {
+//       describe('Noticia inexistente', () => {
     
-    describe('Actualiza una noticia', () => {
-        const newsUpdate = {
-            id: 1,
-            name: 'New title',
-            content: 'New content',
-            image: 'newImageTest.jpg'
-        }
+//       test('Error por id inexistente', async () => {
+//         await request(app)
+//           .get(`/news/${0}`)
+//           .set('x-access-token', token)
+//           .send(news)
+//           .expect(404)
+//           .expect('Content-Type', /application\/json/)
+//           .expect(response => {
+//             expect(response.body.message).toEqual('No se encontró la noticia')
+//         })
+//       })
+//   })
+//  })
 
-        test('Deberia actualizar una noticia', async () => {
-            await request(app)
-            .put(`/news/${newsUpdate.id}`)
-            .set('x-access-token', token)
-            .send(newsUpdate)
+
+// describe('POST /news', () => {
+
+//     describe('Crea una noticia', () => {
+
+//         test('Deberia crear una noticia', async () => {
+//             await request(app)
+//             .post('/news')
+//             .set('x-access-token', token)
+//             .send(news)
+//             .expect(201)
+//             .expect('Content-Type', /application\/json/)
+//             .expect(response => {
+//                 expect(response.body).toBeInstanceOf( Object );
+//             })            
+//         })
+//     })
+
+//     describe('Error por no cumplir con las validaciones', () => {
+
+//         const news = {
+//             name: '',
+//             content: '',
+//             image: ''
+//         }
+
+//         test('Deberia mostrar error por no cumplir con las validaciones', async () => {
+//             await request(app)
+//             .post('/news')
+//             .set('x-access-token', token)
+//             .send(news)
+//             .expect(400)
+//             .expect('Content-Type', /application\/json/)
+//             .expect(response => {
+//                 expect(response.body).toEqual({
+//                     "errors": [
+//                         {
+//                             "value": '',
+//                             "msg": "Image is required",
+//                             "param": "image",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Title is required",
+//                             "param": "name",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Title must be between 3 and 15 characters long",
+//                             "param": "name",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Content is required",
+//                             "param": "content",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Content must be at least 10 characters long",
+//                             "param": "content",
+//                             "location": "body"
+//                         },
+//                       ]
+//                 })                
+//             })             
+//         })
+//     })
+// })
+
+describe('GET /news', () => {
+
+    describe('Muestra todas las noticias', () => {  
+
+        test('Deberia mostrar todas las noticias', async () => {
+        await request(app)
+            .get('/news')
+              .set('x-access-token', tokenPublic) 
+            .send(news)
             .expect(200)
             .expect('Content-Type', /application\/json/)
             .expect(response => {
-                expect(response.body.msg).toEqual('Noticia actualizada')
-            })                      
+                expect(response.body.message).toEqual('ok')
+            })        
+        }) 
     })
-})
 
-    describe('Error por no ser Administrador', () => {
-            
-            test('Deberia mostrar error por no ser Administrador', async () => {
-                await request(app)
-                .put(`/news/${1}`)                
-                .set('x-access-token', tokenPublic)
-                .send(news)
-                .expect(403)
-                .expect('Content-Type', /application\/json/)
-                .expect(response => {
-                    expect(response.body.msg).toEqual('No tiene los permisos necesarios para acceder a esta información, comuniquese con admisntración')                    
-                })                
-            }
-        )
-    }
-    )
-
-    describe('Error por no cumplir con las validaciones', () => {
-
-        const news = {
-            name: '',
-            content: '',
-            image: ''
-        }
-
-        test('Deberia mostrar error por no cumplir con las validaciones', async () => {
+    describe('Error por no encontrar noticias',  () => {
+        const news = null;
+    
+        test('Deberia mostrar error por no encontrar ninguna noticia', async () => {
             await request(app)
-            .put(`/news/${1}`)
-            .set('x-access-token', token)
+            .get('/news')
             .send(news)
-            .expect(400)
+            .expect(200)
             .expect('Content-Type', /application\/json/)
             .expect(response => {
-                expect(response.body).toEqual({
-                    "errors": [
-                        {
-                            "value": '',
-                            "msg": "Image is required",
-                            "param": "image",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Title is required",
-                            "param": "name",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Title must be between 3 and 15 characters long",
-                            "param": "name",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Content is required",
-                            "param": "content",
-                            "location": "body"
-                        },
-                        {
-                            "value": '',
-                            "msg": "Content must be at least 10 characters long",
-                            "param": "content",
-                            "location": "body"
-                        },
-                      ]
-                })                
-            })             
+                // expect(response.body.message).toEqual('ok')
+                (console.log(response.body))
+            })         
         })
     })
 })
+
+
+// describe('PUT /news/:id', () => {
+    
+//     describe('Actualiza una noticia', () => {
+//         const newsUpdate = {
+//             id: 1,
+//             name: 'New title',
+//             content: 'New content',
+//             image: 'newImageTest.jpg'
+//         }
+
+//         test('Deberia actualizar una noticia', async () => {
+//             await request(app)
+//             .put(`/news/${newsUpdate.id}`)
+//             .set('x-access-token', token)
+//             .send(newsUpdate)
+//             .expect(200)
+//             .expect('Content-Type', /application\/json/)
+//             .expect(response => {
+//                 expect(response.body.msg).toEqual('Noticia actualizada')
+//             })                      
+//     })
+// })
+
+//     describe('Error por no ser Administrador', () => {
+            
+//             test('Deberia mostrar error por no ser Administrador', async () => {
+//                 await request(app)
+//                 .put(`/news/${1}`)                
+//                 .set('x-access-token', tokenPublic)
+//                 .send(news)
+//                 .expect(403)
+//                 .expect('Content-Type', /application\/json/)
+//                 .expect(response => {
+//                     expect(response.body.msg).toEqual('No tiene los permisos necesarios para acceder a esta información, comuniquese con admisntración')                    
+//                 })                
+//             }
+//         )
+//     }
+//     )
+
+//     describe('Error por no cumplir con las validaciones', () => {
+
+//         const news = {
+//             name: '',
+//             content: '',
+//             image: ''
+//         }
+
+//         test('Deberia mostrar error por no cumplir con las validaciones', async () => {
+//             await request(app)
+//             .put(`/news/${1}`)
+//             .set('x-access-token', token)
+//             .send(news)
+//             .expect(400)
+//             .expect('Content-Type', /application\/json/)
+//             .expect(response => {
+//                 expect(response.body).toEqual({
+//                     "errors": [
+//                         {
+//                             "value": '',
+//                             "msg": "Image is required",
+//                             "param": "image",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Title is required",
+//                             "param": "name",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Title must be between 3 and 15 characters long",
+//                             "param": "name",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Content is required",
+//                             "param": "content",
+//                             "location": "body"
+//                         },
+//                         {
+//                             "value": '',
+//                             "msg": "Content must be at least 10 characters long",
+//                             "param": "content",
+//                             "location": "body"
+//                         },
+//                       ]
+//                 })                
+//             })             
+//         })
+//     })
+// })
 
 // describe('DELETE /news/:id', () => {
 
